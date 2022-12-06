228afa5b5284c621a9916275983ebb2a6a0c4f29
Clinton Judy <clinton@judy.io>
2022-12-06 09:37:25 -0500

    Helper method to start, clear empties from end of array

--- 05a/script.rb
+++ 05a/script.rb
@@ -1,8 +1,23 @@
 #!/usr/bin/env ruby
 require 'minitest'
 
+class Ship
+  def self.clear_empties_from_end_of_array(array)
+    while array.last.nil? || array.last.strip.empty?
+      array.pop
+    end
+    array
+  end
+end
+
 class ShipTest < MiniTest::Test
 
+  def test_clear_empties_from_end_of_array
+    array_with_nils = ['Z', 'N', nil]
+    array_with_spaces = ['Z', 'N', " "]
+    assert_equal ['Z', 'N'], Ship.clear_empties_from_end_of_array(array_with_nils)
+    assert_equal ['Z', 'N'], Ship.clear_empties_from_end_of_array(array_with_spaces)
+  end
 end
 
 if ARGV[0] == 'test'
