16f4f0a42db8e51154e8a5f587283713ec0eef4a
Clinton Judy <clinton@judy.io>
2022-12-06 21:32:10 -0500

    Day 6 Star 1

--- 06/script.rb
+++ 06/script.rb
@@ -1,12 +1,32 @@
 #!/usr/bin/env ruby
 require 'minitest'
 
-class Test < MiniTest::Test
+class Device
+  def initialize(datastream)
+    @datastream = datastream # must be IO object
+  end
 
+  def find_start_of_packet_marker
+    position = 0
+    possible_marker = []
+    while char = @datastream.gets(1)
+      position += 1
+      possible_marker.shift(1) if possible_marker.length == 4
+      possible_marker << char
+      break if possible_marker.uniq.length == 4
+    end
+    position
+  end
+end
+
+class DeviceTest < MiniTest::Test
+  def test_find_start_of_packet_marker
+    assert_equal 7, Device.new(StringIO.new("mjqjpqmgbljsphdztnvjfqwrcgsmlb")).find_start_of_packet_marker
+  end
 end
 
 if ARGV[0] == 'test'
   MiniTest.run
 else
-  raise "unused"
+  puts Device.new(ARGF).find_start_of_packet_marker
 end
