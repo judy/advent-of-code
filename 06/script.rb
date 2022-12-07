469d32358d464d9f370e891dbb5aa7c7279382bd
Clinton Judy <clinton@judy.io>
2022-12-06 21:34:27 -0500

    Day 6 Star 2

--- 06/script.rb
+++ 06/script.rb
@@ -2,31 +2,32 @@
 require 'minitest'
 
 class Device
+  PACKET_LENGTH = 14
   def initialize(datastream)
     @datastream = datastream # must be IO object
   end
 
-  def find_start_of_packet_marker
+  def find_start_of_message_marker
     position = 0
     possible_marker = []
     while char = @datastream.gets(1)
       position += 1
-      possible_marker.shift(1) if possible_marker.length == 4
+      possible_marker.shift(1) if possible_marker.length == PACKET_LENGTH
       possible_marker << char
-      break if possible_marker.uniq.length == 4
+      break if possible_marker.uniq.length == PACKET_LENGTH
     end
     position
   end
 end
 
 class DeviceTest < MiniTest::Test
-  def test_find_start_of_packet_marker
-    assert_equal 7, Device.new(StringIO.new("mjqjpqmgbljsphdztnvjfqwrcgsmlb")).find_start_of_packet_marker
+  def test_find_start_of_message_marker
+    assert_equal 19, Device.new(StringIO.new("mjqjpqmgbljsphdztnvjfqwrcgsmlb")).find_start_of_message_marker
   end
 end
 
 if ARGV[0] == 'test'
   MiniTest.run
 else
-  puts Device.new(ARGF).find_start_of_packet_marker
+  puts Device.new(ARGF).find_start_of_message_marker
 end
