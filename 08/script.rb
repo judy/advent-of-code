876cbc047d05d3e9997498c1a19c9958ab230081
Clinton Judy <clinton@judy.io>
2022-12-11 18:00:05 -0500

    Part one solves

--- 08/script.rb
+++ 08/script.rb
@@ -12,6 +12,33 @@ class VisibleTrees
     visible_trees = Array.new(forest.size) { Array.new(forest[0].size, false) }
     [forest, visible_trees]
   end
+
+  def externally_visible_trees_count
+    visible_trees.map { _1.count(true) }.sum
+  end
+
+  def solve_axis
+    forest.each_with_index do |row, x|
+      row.each_with_index do |tree, y|
+        next if visible_trees[x][y] # skip if already visible
+
+        left = row[0...y]
+        right = row[y+1..-1]
+        visible_trees[x][y] = left.all? { _1 < tree } || right.all? { _1 < tree }
+      end
+    end
+
+    self
+  end
+
+  def solve
+    solve_axis
+    @forest = forest.transpose
+    @visible_trees = visible_trees.transpose
+    solve_axis
+
+    self
+  end
 end
 
 class Test < MiniTest::Test
@@ -31,8 +58,44 @@ class Test < MiniTest::Test
     assert_equal [[false, false, false], [false, false, false]], VisibleTrees.new(io).visible_trees
   end
 
+  def test_solve_axis
+    io = StringIO.new <<~STRING
+      919
+      191
+      911
+    STRING
+    expected_visible_trees = [
+      [true, false, true],
+      [true, true, true],
+      [true, false, true]
+    ]
+    assert_equal expected_visible_trees, VisibleTrees.new(io).solve_axis.visible_trees
+  end
+
+  def test_externally_visible_trees_count
+    io = StringIO.new <<~STRING
+      919
+      191
+      911
+    STRING
+    assert_equal 7, VisibleTrees.new(io).solve_axis.externally_visible_trees_count
+  end
+
+  def test_solve
+    io = StringIO.new <<~STRING
+      919
+      919
+      919
+    STRING
+    expected_visible_trees = [
+      [true, true, true],
+      [true, false, true],
+      [true, true, true]
+    ]
+    assert_equal expected_visible_trees, VisibleTrees.new(io).solve.visible_trees
+  end
+
   def test_example
-    skip
     io = StringIO.new <<~STRING
       30373
       25512
@@ -41,13 +104,12 @@ class Test < MiniTest::Test
       35390
     STRING
     expected = 21
-    assert_equal expected, VisibleTrees.new(io).solve
+    assert_equal expected, VisibleTrees.new(io).solve.externally_visible_trees_count
   end
 end
 
 if ARGV[0] == 'test'
   MiniTest.run
 else
-  raise "unused"
-  # puts Solution.new(ARGF).solve
+  puts VisibleTrees.new(ARGF).solve.externally_visible_trees_count
 end
