cde502e4de1ba6ee898521b900ee816c5258d7b7
Clinton Judy <clinton@judy.io>
2022-12-11 18:56:53 -0500

    Skip to day 9 part 1

---
+++ 09/script.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+require 'minitest'
+
+# Solution goes here
+
+class Test < MiniTest::Test
+  def test_example
+    skip
+    io = StringIO.new <<~STRING
+      # example input goes here
+    STRING
+    expected = "example output goes here"
+  end
+end
+
+if ARGV[0] == 'test'
+  MiniTest.run
+else
+  raise "unused"
+  # puts Solution.new(ARGF).solve
+end
