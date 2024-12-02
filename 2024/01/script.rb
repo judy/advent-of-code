2aa5f74db89ac822f2ea969c831ca78e0132024c
Clinton Judy <clinton@judy.io>
2024-12-02 11:16:21 -0500

    2024-01 - Gold star 2

--- 2024/01/script.rb
+++ 2024/01/script.rb
@@ -10,7 +10,7 @@ class Solution
     @io = io
   end
 
-  def solve
+  def solve_one
     # Convert file to nested array
     arrays = @io.each_line.map do |line|
       line.split.map(&:to_i)
@@ -27,13 +27,39 @@ class Solution
 
     distance
   end
+
+  def solve_two
+    # Convert file to nested array
+    arrays = @io.each_line.map do |line|
+      line.split.map(&:to_i)
+    end
+
+    # Transpose and sort each array
+    array_1, array_2 = arrays.transpose.map(&:sort)
+
+    # Iterate through each array and compare, adding up total distance
+    distance = 0
+    array_1.each_with_index do |num, index|
+      multiplier = array_2.count(num)
+      distance += num * multiplier
+    end
+
+    distance
+  end
 end
 
 class SolutionTest < Minitest::Test
-  def test_example
+  def test_example_one
     io = File.open(__dir__ + '/sample.txt')
-    solution = Solution.new(io).solve
+    solution = Solution.new(io).solve_one
     expected = 11
+    assert_equal expected, solution
+  end
+
+  def test_example_two
+    io = File.open(__dir__ + '/sample.txt')
+    solution = Solution.new(io).solve_two
+    expected = 31
     assert_equal expected, solution
   end
 end
@@ -41,5 +67,5 @@ end
 if ARGV[0] == 'test'
   Minitest.run
 else
-  puts Solution.new(ARGF).solve
+  puts Solution.new(ARGF).solve_two
 end
