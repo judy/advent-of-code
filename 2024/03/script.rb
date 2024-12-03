d326cfb6937773eeaf5fc4c39ccc4ca15193c0a5
Clinton Judy <clinton@judy.io>
2024-12-03 11:35:46 -0500

    2024-03 - Gold star 2

--- 2024/03/script.rb
+++ 2024/03/script.rb
@@ -5,17 +5,33 @@ require 'pry'
 require 'pry-byebug'
 
 class Solution
+  MUL_REGEX = /mul\((?<a>\d{1,3}),(?<b>\d{1,3})\)/
   attr_reader :io
   def initialize(io)
     @io = io
   end
 
-  def solve
+  def solve_simple
     code = io.readlines.map(&:strip).join
-    re = /mul\((?<a>\d{1,3}),(?<b>\d{1,3})\)/
 
     total = 0
-    code.scan(re) do |a, b|
+    code.scan(MUL_REGEX) do |a, b|
+      a = a.to_i
+      b = b.to_i
+      total += a * b
+    end
+
+    total
+  end
+
+  def solve
+    code = io.readlines.map(&:strip).join
+
+    # delete blocks of don't to do. .*? means lazy. also delete block at end if don't doesn't reach a do
+    code.gsub!(/don\'t\(\).*?do\(\)|don\'t\(\).*?$/, '')
+
+    total = 0
+    code.scan(MUL_REGEX) do |a, b|
       a = a.to_i
       b = b.to_i
       total += a * b
@@ -26,10 +42,17 @@ class Solution
 end
 
 class SolutionTest < Minitest::Test
+  def test_simple_example
+    io = File.open(__dir__ + '/sample.txt')
+    solution = Solution.new(io).solve_simple
+    expected = 161
+    assert_equal expected, solution
+  end
+
   def test_example
     io = File.open(__dir__ + '/sample.txt')
     solution = Solution.new(io).solve
-    expected = 161 # TODO: put example here!
+    expected = 48
     assert_equal expected, solution
   end
 end
