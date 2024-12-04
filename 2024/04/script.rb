c527670dce8146c53ab88af71d1355e6d6f8aa43
Clinton Judy <clinton@judy.io>
2024-12-04 09:44:13 -0500

    2024-04 - Start

---
+++ 2024/04/script.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+require 'minitest'
+require 'minitest/focus'
+require 'pry'
+require 'pry-byebug'
+
+class Solution
+  attr_reader :io
+  def initialize(io)
+    @io = io
+  end
+
+  def solve
+    # TODO: solve here!
+  end
+end
+
+class SolutionTest < Minitest::Test
+  def test_example
+    io = File.open(__dir__ + '/sample.txt')
+    solution = Solution.new(io).solve
+    expected = 18
+    assert_equal expected, solution
+  end
+end
+
+if ARGV[0] == 'test'
+  Minitest.run
+else
+  puts Solution.new(ARGF).solve
+end
