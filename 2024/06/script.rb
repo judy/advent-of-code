b9f333b32dcfa888d7ccf9f04374ffd8a732530a
Clinton Judy <clinton@judy.io>
2024-12-06 23:23:36 -0500

    2024-06 - Gold star 1

--- 2024/06/script.rb
+++ 2024/06/script.rb
@@ -3,15 +3,151 @@ require 'minitest'
 require 'minitest/focus'
 require 'pry'
 require 'pry-byebug'
+require 'tty-cursor'
+
+Guard = Struct.new(:x, :y, :direction, :on_map)
+
+class Map
+  include Enumerable
+
+  attr_reader :map
+
+  def initialize(map)
+    @map = map
+  end
+
+  def each(&block)
+    map.each(&block)
+  end
+
+  def width
+    map.first.size
+  end
+
+  def height
+    map.size
+  end
+
+  def at(x, y)
+    return nil if x < 0
+    return nil if y < 0
+    return nil if x >= width
+    return nil if y >= height
+
+    map[y][x]
+  end
+
+  def set(x, y, value)
+    map[y][x] = value
+  end
+end
 
 class Solution
-  attr_reader :io
+  attr_reader :io, :map, :guard, :cursor, :timeout
   def initialize(io)
     @io = io
+    @map = Map.new(io.each_line.map(&:chomp).map(&:chars))
+    @cursor = TTY::Cursor
+    @timeout = 0
   end
 
   def solve
-    # TODO: solve here!
+    print cursor.clear_screen_down
+    @guard = set_guard
+
+    while guard.on_map
+      @timeout += 1
+      draw_map
+      move_guard
+      guard.on_map = false if timeout > 100_000_000 # TODO: Remove this sanity check
+    end
+
+    count_squares = count_path_squares
+    puts "Path squares: #{count_squares}"
+    count_squares
+  end
+
+  def move_guard
+    if guard.direction == :up
+      if map.at(guard.x, guard.y - 1) == '#'
+        guard.direction = :right
+      elsif map.at(guard.x, guard.y - 1).nil?
+        guard.on_map = false
+        return
+      else
+        map.set(guard.x, guard.y, 'X')
+        guard.y -= 1
+        map.set(guard.x, guard.y, '^')
+        return
+      end
+    end
+
+    if guard.direction == :right
+      if map.at(guard.x + 1, guard.y) == '#'
+        guard.direction = :down
+      elsif map.at(guard.x + 1, guard.y).nil?
+        guard.on_map = false
+        return
+      else
+        map.set(guard.x, guard.y, 'X')
+        guard.x += 1
+        map.set(guard.x, guard.y, '>')
+        return
+      end
+    end
+
+    if guard.direction == :down
+      if map.at(guard.x, guard.y + 1) == '#'
+        guard.direction = :left
+      elsif map.at(guard.x, guard.y + 1).nil?
+        guard.on_map = false
+        return
+      else
+        map.set(guard.x, guard.y, 'X')
+        guard.y += 1
+        map.set(guard.x, guard.y, 'V')
+        return
+      end
+    end
+
+    if guard.direction == :left
+      if map.at(guard.x - 1, guard.y) == '#'
+        guard.direction = :up
+      elsif map.at(guard.x - 1, guard.y).nil?
+        guard.on_map = false
+        return
+      else
+        map.set(guard.x, guard.y, 'X')
+        guard.x -= 1
+        map.set(guard.x, guard.y, '<')
+        return
+      end
+    end
+  end
+
+  def draw_map
+    print cursor.save
+    print cursor.down(4)
+    puts "Guard: #{sprintf("%3d", guard.x)}, #{sprintf("%3d", guard.y)}  Timeout: #{sprintf("%6d", timeout)}"
+    map.each do |row|
+      puts row.join
+    end
+    print cursor.restore
+  end
+
+  def count_path_squares
+    # add one at the end for going off screen :p
+    map.inject(0) {|sum, row| row.count('X') + sum} + 1
+  end
+
+  def set_guard
+    map.each_with_index do |row, y|
+      row.each_with_index do |char, x|
+        if char == '^'
+          return Guard.new(x, y, :up, true)
+        end
+      end
+    end
   end
 end
 
@@ -19,8 +155,16 @@ class SolutionTest < Minitest::Test
   def test_example
     io = File.open(__dir__ + '/sample.txt')
     solution = Solution.new(io).solve
-    expected = "???" # TODO: put example here!
+    expected = 41
     assert_equal expected, solution
+  end
+
+  def test_set_guard
+    io = File.open(__dir__ + '/sample.txt')
+    solution = Solution.new(io)
+    guard = solution.set_guard
+    assert_equal 4, guard.x
+    assert_equal 6, guard.y
   end
 end
 
