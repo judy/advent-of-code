5f9951c1cfbb1c3607cd0b43d7cb18693a07f290
Clinton Judy <clinton@zipline.inc>
2025-12-01 14:37:58 -0500

    2025-01 - Gold star 1

--- 2025/01/script.rb
+++ 2025/01/script.rb
@@ -11,8 +11,25 @@ class Solution
   end
 
   def solve
-    @io.each_line.do |line|
+    solution = 0
+    dial = 50
+
+    # # To match the example output exactly:
+    # puts "- The dial starts by pointing at #{dial}."
+
+    @io.each_line do |line|
+      direction, amount = line[0], line[1..].to_i
+      raise "Unknown direction #{direction}" unless ['L', 'R'].include?(direction)
+
+      amount *= -1 if direction == 'L'
+      dial = (dial + amount) % 100
+
+      # # To match the example output exactly:
+      # puts "- The dial is rotated #{line.strip} to point at #{dial}."
+      solution += 1 if dial == 0
     end
+
+    solution
   end
 end
 
@@ -20,7 +37,7 @@ class SolutionTest < Minitest::Test
   def test_example
     io = File.open(__dir__ + '/sample.txt')
     solution = Solution.new(io).solve
-    expected = "???" # TODO: put example here!
+    expected = 3
     assert_equal expected, solution
   end
 end
