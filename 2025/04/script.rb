1b1f686577d27ab93f615edb7d4150d6a398e9b4
Clinton Judy <clinton@judy.io>
2025-12-13 11:23:49 -0500

    2025-04 - Simple grid implementation

--- 2025/04/script.rb
+++ 2025/04/script.rb
@@ -4,14 +4,49 @@ require 'minitest/focus'
 require 'pry'
 require 'pry-byebug'
 
+class Grid
+  include Enumerable
+
+  def initialize(io)
+    @grid = io.each_line.map.with_index do |line, y|
+      line.chomp.chars.map.with_index do |char, x|
+        { char:, x:, y: }
+      end
+    end
+  end
+
+  def to_s
+    @grid.map do |row|
+      row.map { |cell| cell[:char] }.join
+    end.join("\n")
+  end
+
+  def each
+    @grid.each do |row|
+      row.each do |cell|
+        yield cell
+      end
+    end
+  end
+end
+
 class Solution
-  attr_reader :io
+  attr_reader :io, :grid
   def initialize(io)
     @io = io
+    @grid = Grid.new(io)
   end
 
   def solve
-    # TODO: solve here!
+    puts @grid
+  end
+end
+
+class GridTest < Minitest::Test
+  def test_count
+    io = File.open(__dir__ + '/sample.txt')
+    grid = Grid.new(io)
+    assert_equal 100, grid.count
   end
 end
 
