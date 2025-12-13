3e35b7df711fff5f3fa7b344cdb996c1ebadf78c
Clinton Judy <clinton@judy.io>
2025-12-13 13:49:00 -0500

    Ranges are nicer. Partition is awesome

--- 2025/05/script.rb
+++ 2025/05/script.rb
@@ -7,28 +7,22 @@ require 'pry-byebug'
 class Solution
   attr_reader :ranges, :ingredients
   def initialize(io)
-    @ranges = []
-    @ingredients = []
-
-    # get ranges
-    while true
-      line = io.gets&.chomp
-      break if line == ""
-      @ranges << line.split('-').map(&:to_i)
+    @ranges, @ingredients = io.partition { |line| line.include?("-") }
+    @ranges.collect! do |range|
+      min, max = range.split("-").collect(&:to_i)
+      (min..max)
     end
+    @ingredients = @ingredients.map(&:chomp).reject(&:empty?).map(&:to_i)
+  end
 
-    # get ingredients
-    while true
-      line = io.gets&.chomp
-      break if line.nil?
-      @ingredients << line.to_i
+  def solve_star_one
+    ingredients.count do |ingredient|
+      ranges.any? { |range| range.include?(ingredient ) }
     end
   end
 
-  def solve
-    ingredients.sum do |ingredient|
-      ranges.any? { |range| ingredient >= range[0] && ingredient <= range[1] } ? 1 : 0
-    end
+  def solve_star_two
+
   end
 end
 
@@ -36,14 +30,21 @@ class SolutionTest < Minitest::Test
   def test_solution_initialization
     io = File.open(__dir__ + '/sample.txt')
     solution = Solution.new(io)
-    assert_equal [[3, 5], [10, 14], [16, 20], [12, 18]], solution.ranges
+    assert_equal [3..5, 10..14, 16..20, 12..18], solution.ranges
     assert_equal [1, 5, 8, 11, 17, 32], solution.ingredients
   end
 
-  def test_example
+  def test_solve_star_one
     io = File.open(__dir__ + '/sample.txt')
-    solution = Solution.new(io).solve
+    solution = Solution.new(io).solve_star_one
     expected = 3
+    assert_equal expected, solution
+  end
+
+  def test_solve_star_two
+    io = File.open(__dir__ + '/sample.txt')
+    solution = Solution.new(io).solve_star_two
+    expected = 14
     assert_equal expected, solution
   end
 end
@@ -51,5 +52,5 @@ end
 if ARGV[0] == 'test'
   Minitest.run
 else
-  puts Solution.new(ARGF).solve
+  puts Solution.new(ARGF).solve_star_two
 end
