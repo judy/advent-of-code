c3f62a8e56abd54b951bcb4adfe32ca43b0a7215
Clinton Judy <clinton@judy.io>
2025-12-13 13:34:21 -0500

    Parse data

--- 2025/05/script.rb
+++ 2025/05/script.rb
@@ -5,17 +5,39 @@ require 'pry'
 require 'pry-byebug'
 
 class Solution
-  attr_reader :io
+  attr_reader :ranges, :ingredients
   def initialize(io)
-    @io = io
+    @ranges = []
+    @ingredients = []
+
+    # get ranges
+    while true
+      line = io.gets&.chomp
+      break if line == ""
+      @ranges << line.split('-').map(&:to_i)
+    end
+
+    # get ingredients
+    while true
+      line = io.gets&.chomp
+      break if line.nil?
+      @ingredients << line.to_i
+    end
   end
 
   def solve
-    # TODO: solve here!
+    0
   end
 end
 
 class SolutionTest < Minitest::Test
+  def test_solution_initialization
+    io = File.open(__dir__ + '/sample.txt')
+    solution = Solution.new(io)
+    assert_equal [[3, 5], [10, 14], [16, 20], [12, 18]], solution.ranges
+    assert_equal [1, 5, 8, 11, 17, 32], solution.ingredients
+  end
+
   def test_example
     io = File.open(__dir__ + '/sample.txt')
     solution = Solution.new(io).solve
